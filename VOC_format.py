3ccca6e5fcfe99214989130d98ac262b44387fbf
Jane Hung <jyhung@broadinstitute.org>
2016-02-11 16:04:14 -0500

    Update VOC_format.py

--- VOC_format.py
+++ VOC_format.py
@@ -11,12 +11,22 @@ import numpy as np
 Randomly takes X subsamples of full image and outputs in other folder
 Usage: python VOC_format.py [Output directory] [Image files]
 '''
+
+#if file exists, remove
+def removeIfExists(output_dir, subdir, name): 
+    filename = os.path.join(output_dir, subdir, name)
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+    return filename
+    
 IGNORE_EDGE_CELLS = True
 output_dir = argv[1:]#os.path.join('/Users', 'jyhung', 'Documents', 'VOC_format', 'data')
 print 'output director', output_dir
-num_subimages = 10
+num_subimages = 50
 print 'number of subimages', num_subimages
-small_size = 400
+small_size = 224
 print 'size of subimages (px)', small_size
 
 #clear existing files
@@ -27,12 +37,7 @@ for name in ['Annotations', 'ImageSets', 'Images']:
 
 #if train.txt or test.txt exists, remove
 for train_or_test in ['train', 'text']:
-    filename_train = os.path.join(output_dir, 'ImageSets',train_or_test+'.txt')
-    #print 'train or test:%s'%train_or_test
-    try:
-        os.remove(filename_train)
-    except OSError:
-        pass
+    filename_train = removeIfExists(output_dir, 'ImageSets', train_or_test+'.txt')
 
 #for each image, subsample image and for each subimage, create associated file with bounding box and class information
 filenum = 1
@@ -67,12 +72,13 @@ for filename in argv[2:]:
     data = []
     for object in root.findall('object'):
         label = object.find('name').text
-        #if IGNORE_EDGE_CELLS is true, skip object
+        #if label is empty, skip
+        #if IGNORE_EDGE_CELLS is true and label begins with e, skip object (CONTINUE NOT BREAK)
         #if label starts with e (cell is on the edge), relabel without e
         if not label:
-            break
+            continue
         elif IGNORE_EDGE_CELLS and label[0] == 'e':
-            break
+            continue
         elif label[0] == 'e':
             label = label[1:]
         try:
@@ -101,61 +107,64 @@ for filename in argv[2:]:
             raise Exception('object data ', object_data)
         data.append(object_data)
 
-
-    for sub in range(num_subimages):
-        print sub
-        subname = os.path.basename(file_)+'_'+str(sub)
-        #randomly choose top left corner of subimage
-        randx = random.randint(0, width-small_size)
-        randy = random.randint(0, height-small_size)
-        print('top left corner coordinates:%s,%s'%(randx, randy))
-        #save cropped image
-        cropped = img.crop((randx, randy, randx+small_size, randy+small_size))
-
-        #if Annotation file exists, remove
-        filename_annotation = os.path.join(output_dir, 'Annotations',subname+'.txt')
-        try:
-            os.remove(filename_annotation)
-        except OSError:
-            pass
-
-        #write and save annotation file, only including data that are within the bounds of the subimage
-        empty = True
-        for object_data in data:
-            adjusted_data = np.array(object_data[0:-1]).copy()
-            #adjust according to top left corner
-            adjusted_data = adjusted_data - np.array([randx, randy, randx, randy])#map(operator.sub, map(int, adjusted_data), [randx, randy, randx, randy])
-
-            #write if all coordinates are inside the subimage
-            #adjust if 1 or 2 coordinates are inside
-            adjusted_data_x = np.append(adjusted_data[0], adjusted_data[2])
-            adjusted_data_y = np.append(adjusted_data[1], adjusted_data[3])
-            if IGNORE_EDGE_CELLS:
-                if np.all(adjusted_data >= 0) and np.all(adjusted_data < small_size):
-                    empty = False
-                    with open(filename_annotation, 'a') as fp:
-                        for datum in adjusted_data:
-                            fp.write(str(datum)+' ')
-                        print object_data, adjusted_data, object_data[-1]
-                        fp.write(str(object_data[-1])+'\n')
-            else:
-                if not (np.all(adjusted_data_x < 0) or np.all(adjusted_data_y >= small_size) or
-                            np.all(adjusted_data_x >= small_size) or np.all(adjusted_data_y < 0)):
-                    with open(filename_annotation, 'a') as fp:
-                        if np.any(adjusted_data >= small_size):
-                            adjusted_data = np.minimum(adjusted_data, small_size)#map(min, adjusted_data, [228]*4)
-                        if np.any(adjusted_data < 0):
-                            adjusted_data = np.maximum(adjusted_data, 0)#map(max, adjusted_data, [0]*4)
-                        for datum in adjusted_data:
-                            fp.write(str(datum)+' ')
-                        print object_data, adjusted_data, object_data[-1]
-                        fp.write(str(object_data[-1])+'\n')
+    if train_or_test  == 'train':
+        for sub in range(num_subimages):
+            print sub
+            subname = os.path.basename(file_)+'_'+str(sub)
+            #randomly choose top left corner of subimage
+            randx = random.randint(0, width-small_size)
+            randy = random.randint(0, height-small_size)
+            print('top left corner coordinates:%s,%s'%(randx, randy))
+            #save cropped image
+            cropped = img.crop((randx, randy, randx+small_size, randy+small_size))
+    
+            #if Annotation file exists, remove
+            filename_annotation = removeIfExists(output_dir, 'Annotations', subnames+'.txt')
+    
+            #write and save annotation file, only including data that are within the bounds of the subimage
+            empty = True
+            for object_data in data:
+                adjusted_data = np.array(object_data[0:-1]).copy()
+                #adjust according to top left corner
+                adjusted_data = adjusted_data - np.array([randx, randy, randx, randy])#map(operator.sub, map(int, adjusted_data), [randx, randy, randx, randy])
+    
+                #write if all coordinates are inside the subimage
+                #adjust if 1 or 2 coordinates are inside
+                adjusted_data_x = np.append(adjusted_data[0], adjusted_data[2])
+                adjusted_data_y = np.append(adjusted_data[1], adjusted_data[3])
+                if IGNORE_EDGE_CELLS:
+                    if np.all(adjusted_data >= 0) and np.all(adjusted_data < small_size):
                         empty = False
-        #if annotation file not empty
-        #save cropped image name in train.txt file and cropped image
-        if not empty:
-            with open(filename_train, 'a') as fp:
-                fp.write(subname+'\n')
-            cropped.save(os.path.join(output_dir, 'Images', subname+file_extension))
-
-
+                        with open(filename_annotation, 'a') as fp:
+                            for datum in adjusted_data:
+                                fp.write(str(datum)+' ')
+                            print object_data, adjusted_data, object_data[-1]
+                            fp.write(str(object_data[-1])+'\n')
+                else:
+                    if not (np.all(adjusted_data_x < 0) or np.all(adjusted_data_y >= small_size) or
+                                np.all(adjusted_data_x >= small_size) or np.all(adjusted_data_y < 0)):
+                        with open(filename_annotation, 'a') as fp:
+                            if np.any(adjusted_data >= small_size):
+                                adjusted_data = np.minimum(adjusted_data, small_size)#map(min, adjusted_data, [228]*4)
+                            if np.any(adjusted_data < 0):
+                                adjusted_data = np.maximum(adjusted_data, 0)#map(max, adjusted_data, [0]*4)
+                            for datum in adjusted_data:
+                                fp.write(str(datum)+' ')
+                            print object_data, adjusted_data, object_data[-1]
+                            fp.write(str(object_data[-1])+'\n')
+                            empty = False
+            #if annotation file not empty
+            #save cropped image name in train.txt file and cropped image
+            if not empty:
+                with open(filename_train, 'a') as fp:
+                    fp.write(subname+'\n')
+                cropped.save(os.path.join(output_dir, 'Images', subname+file_extension))
+    elif test_or_train == 'test': #full image with all annotations 
+        #if Annotation file exists, remove
+        filename_annotation = removeIfExists(output_dir, 'Annotations', os.path.basename(file_)+'.txt')
+        for object_data in data:
+            with open(filename_annotation, 'a') as fp:
+                for datum in object_data:
+                    fp.write(str(datum)+' ')
+                fp.write(str(object_data[-1])+'\n')
+                        
