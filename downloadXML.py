7c93636efa1c3679767938650be89721f7dc099a
Jane Hung <jyhung@broadinstitute.org>
2016-03-24 18:59:10 -0400

    Create downloadXML.py

---
+++ downloadXML.py
@@ -0,0 +1,5 @@
+###
+For specified files, replace the files in the current folder with ones downloaded from the internet 
+python downloadXML.py /home/jyhung/Downloads/collection/Annotations/users/jane24/54gamplusring_20151223_outlines/
+###
+
