6cb26813e34ab2f28a0d89fc356612e367ae9d2b
Jane Hung <jyhung@broadinstitute.org>
2016-07-29 19:58:58 +0800

    Update VOC_format3.py

--- VOC_format3.py
+++ VOC_format3.py
@@ -43,7 +43,8 @@ def extractObjectData(obj):
         print 'polygon', polygon.findall('pt')
         for pt in polygon.findall('pt'):
         	for ppx in pt.findall('x'):
-        		print ppx.text
+        		print type(ppx.text)
+        		
         
         x = [int(px.text) for px in pt.findall('x') for pt in polygon.findall('pt')]
         y = [int(py.text) for py in pt.findall('y') for pt in polygon.findall('pt')]
